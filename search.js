596e709f328224ba7c88492a0a46ed2ad8247643
Sofia <sofiaprez523@gmail.com>
2020-12-14 16:34:18 -0800

    intial commit

---
+++ search.js
@@ -0,0 +1,272 @@
+function search(){
+	searchM1();
+	searchM2();
+	searchM3();
+	searchM4();
+	searchM5();
+    searchM6();
+	searchM7();
+	searchM8();
+	searchM9();
+	searchM10();
+    
+	searchH1();
+	searchH2();
+	searchH3();
+	searchH4();
+	searchH5();
+    searchH6();
+	searchH7();
+	searchH8();
+	searchH9();
+	searchH10();
+}
+
+
+function searchM1() {
+  var str = document.getElementById('mediumA1').value;
+
+  if (str.includes("slideshow") || str.includes("Slideshow") || str.includes("SLIDESHOW")) {
+     document.getElementById("slideshowHelpBtn1").style.display = "block";
+  } else if (str.includes("math") || str.includes("Math") || str.includes("MATH")) {
+  	document.getElementById("mathHelpBtn1").style.display = "block";
+  }
+
+}
+
+
+function searchM2() {
+  var str = document.getElementById('mediumA2').value;
+
+  if (str.includes("slideshow") || str.includes("Slideshow") || str.includes("SLIDESHOW")) {
+     document.getElementById("slideshowHelpBtn2").style.display = "block";
+  } else if (str.includes("math") || str.includes("Math") || str.includes("MATH")) {
+  	document.getElementById("mathHelpBtn2").style.display = "block";
+  }
+
+}
+
+
+function searchM3() {
+  var str = document.getElementById('mediumA3').value;
+
+  if (str.includes("slideshow") || str.includes("Slideshow") || str.includes("SLIDESHOW")) {
+     document.getElementById("slideshowHelpBtn3").style.display = "block";
+  } else if (str.includes("math") || str.includes("Math") || str.includes("MATH")) {
+  	document.getElementById("mathHelpBtn3").style.display = "block";
+  }
+
+}
+
+
+function searchM4() {
+  var str = document.getElementById('mediumA4').value;
+
+  if (str.includes("slideshow") || str.includes("Slideshow") || str.includes("SLIDESHOW")) {
+     document.getElementById("slideshowHelpBtn4").style.display = "block";
+  } else if (str.includes("math") || str.includes("Math") || str.includes("MATH")) {
+  	document.getElementById("mathHelpBtn4").style.display = "block";
+  }
+
+}
+
+
+
+function searchM5() {
+  var str = document.getElementById('mediumA5').value;
+
+  if (str.includes("slideshow") || str.includes("Slideshow") || str.includes("SLIDESHOW")) {
+     document.getElementById("slideshowHelpBtn5").style.display = "block";
+  } else if (str.includes("math") || str.includes("Math") || str.includes("MATH")) {
+  	document.getElementById("mathHelpBtn5").style.display = "block";
+  }
+
+}
+
+
+function searchM6() {
+  var str = document.getElementById('mediumA6').value;
+
+  if (str.includes("slideshow") || str.includes("Slideshow") || str.includes("SLIDESHOW")) {
+     document.getElementById("slideshowHelpBtn6").style.display = "block";
+  } else if (str.includes("math") || str.includes("Math") || str.includes("MATH")) {
+  	document.getElementById("mathHelpBtn6").style.display = "block";
+  }
+
+}
+
+
+function searchM7() {
+  var str = document.getElementById('mediumA7').value;
+
+  if (str.includes("slideshow") || str.includes("Slideshow") || str.includes("SLIDESHOW")) {
+     document.getElementById("slideshowHelpBtn7").style.display = "block";
+  } else if (str.includes("math") || str.includes("Math") || str.includes("MATH")) {
+  	document.getElementById("mathHelpBtn7").style.display = "block";
+  }
+
+}
+
+
+
+function searchM8() {
+  var str = document.getElementById('mediumA8').value;
+
+  if (str.includes("slideshow") || str.includes("Slideshow") || str.includes("SLIDESHOW")) {
+     document.getElementById("slideshowHelpBtn8").style.display = "block";
+  } else if (str.includes("math") || str.includes("Math") || str.includes("MATH")) {
+  	document.getElementById("mathHelpBtn8").style.display = "block";
+  }
+
+}
+
+
+function searchM9() {
+  var str = document.getElementById('mediumA9').value;
+
+  if (str.includes("slideshow") || str.includes("Slideshow") || str.includes("SLIDESHOW")) {
+     document.getElementById("slideshowHelpBtn9").style.display = "block";
+  } else if (str.includes("math") || str.includes("Math") || str.includes("MATH")) {
+  	document.getElementById("mathHelpBtn9").style.display = "block";
+  }
+
+}
+
+function searchM10() {
+  var str = document.getElementById('mediumA10').value;
+
+  if (str.includes("slideshow") || str.includes("Slideshow") || str.includes("SLIDESHOW")) {
+     document.getElementById("slideshowHelpBtn10").style.display = "block";
+  } else if (str.includes("math") || str.includes("Math") || str.includes("MATH")) {
+  	document.getElementById("mathHelpBtn10").style.display = "block";
+  }
+
+}
+
+
+
+
+// hard
+
+
+
+function searchH1() {
+  var str = document.getElementById('hardA1').value;
+
+  if (str.includes("slideshow") || str.includes("Slideshow") || str.includes("SLIDESHOW")) {
+     document.getElementById("slideshowHelpBtn11").style.display = "block";
+  } else if (str.includes("math") || str.includes("Math") || str.includes("MATH")) {
+  	document.getElementById("mathHelpBtn11").style.display = "block";
+  }
+
+}
+
+
+function searchH2() {
+  var str = document.getElementById('hardA2').value;
+
+  if (str.includes("slideshow") || str.includes("Slideshow") || str.includes("SLIDESHOW")) {
+     document.getElementById("slideshowHelpBtn12").style.display = "block";
+  } else if (str.includes("math") || str.includes("Math") || str.includes("MATH")) {
+  	document.getElementById("mathHelpBtn12").style.display = "block";
+  }
+
+}
+
+
+function searchH3() {
+  var str = document.getElementById('hardA3').value;
+
+  if (str.includes("slideshow") || str.includes("Slideshow") || str.includes("SLIDESHOW")) {
+     document.getElementById("slideshowHelpBtn13").style.display = "block";
+  } else if (str.includes("math") || str.includes("Math") || str.includes("MATH")) {
+  	document.getElementById("mathHelpBtn13").style.display = "block";
+  }
+
+}
+
+
+function searchH4() {
+  var str = document.getElementById('hardA4').value;
+
+  if (str.includes("slideshow") || str.includes("Slideshow") || str.includes("SLIDESHOW")) {
+     document.getElementById("slideshowHelpBtn14").style.display = "block";
+  } else if (str.includes("math") || str.includes("Math") || str.includes("MATH")) {
+  	document.getElementById("mathHelpBtn14").style.display = "block";
+  }
+
+}
+
+
+
+function searchH5() {
+  var str = document.getElementById('hardA5').value;
+
+  if (str.includes("slideshow") || str.includes("Slideshow") || str.includes("SLIDESHOW")) {
+     document.getElementById("slideshowHelpBtn15").style.display = "block";
+  } else if (str.includes("math") || str.includes("Math") || str.includes("MATH")) {
+  	document.getElementById("mathHelpBtn15").style.display = "block";
+  }
+
+}
+
+
+function searchH6() {
+  var str = document.getElementById('hardA6').value;
+
+  if (str.includes("slideshow") || str.includes("Slideshow") || str.includes("SLIDESHOW")) {
+     document.getElementById("slideshowHelpBtn16").style.display = "block";
+  } else if (str.includes("math") || str.includes("Math") || str.includes("MATH")) {
+  	document.getElementById("mathHelpBtn16").style.display = "block";
+  }
+
+}
+
+
+function searchH7() {
+  var str = document.getElementById('hardA7').value;
+
+  if (str.includes("slideshow") || str.includes("Slideshow") || str.includes("SLIDESHOW")) {
+     document.getElementById("slideshowHelpBtn17").style.display = "block";
+  } else if (str.includes("math") || str.includes("Math") || str.includes("MATH")) {
+  	document.getElementById("mathHelpBtn17").style.display = "block";
+  }
+
+}
+
+
+
+function searchH8() {
+  var str = document.getElementById('hardA8').value;
+
+  if (str.includes("slideshow") || str.includes("Slideshow") || str.includes("SLIDESHOW")) {
+     document.getElementById("slideshowHelpBtn18").style.display = "block";
+  } else if (str.includes("math") || str.includes("Math") || str.includes("MATH")) {
+  	document.getElementById("mathHelpBtn18").style.display = "block";
+  }
+
+}
+
+
+function searchH9() {
+  var str = document.getElementById('hardA9').value;
+
+  if (str.includes("slideshow") || str.includes("Slideshow") || str.includes("SLIDESHOW")) {
+     document.getElementById("slideshowHelpBtn19").style.display = "block";
+  } else if (str.includes("math") || str.includes("Math") || str.includes("MATH")) {
+  	document.getElementById("mathHelpBtn19").style.display = "block";
+  }
+
+}
+
+function searchH10() {
+  var str = document.getElementById('hardA10').value;
+
+  if (str.includes("slideshow") || str.includes("Slideshow") || str.includes("SLIDESHOW")) {
+     document.getElementById("slideshowHelpBtn20").style.display = "block";
+  } else if (str.includes("math") || str.includes("Math") || str.includes("MATH")) {
+  	document.getElementById("mathHelpBtn20").style.display = "block";
+  }
+
+}
+
